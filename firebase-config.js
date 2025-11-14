00ce9a138662a2c4fc7b8ab25c531cb8b480b879
inglagattuta <ing.lagattuta@gmail.com>
2025-11-14 11:56:41 +0100

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration details for the project.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhDoRKmRffrjO-WvVjgX3K7JdfPaM7MGk",
+  authDomain: "portafoglio-dashboard.firebaseapp.com",
+  projectId: "portafoglio-dashboard",
+  storageBucket: "portafoglio-dashboard.firebasestorage.app",
+  messagingSenderId: "194509041146",
+  appId: "1:194509041146:web:aa943d555dc067f7110843"
+};
