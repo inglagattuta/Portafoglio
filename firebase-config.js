ff07c4ba08b72b8ef26c0a16379735438519e372
inglagattuta <ing.lagattuta@gmail.com>
2025-11-19 20:46:56 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 
-
-
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// 🔹 Sostituisci con il tuo firebaseConfig
+// 👉 QUI METTI LA TUA CONFIG FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBhDoRKmRffrjO-WvVjgX3K7JdfPaM7MGk",
+   apiKey: "AIzaSyBhDoRKmRffrjO-WvVjgX3K7JdfPaM7MGk",
   authDomain: "portafoglio-dashboard.firebaseapp.com",
   projectId: "portafoglio-dashboard",
   storageBucket: "portafoglio-dashboard.firebasestorage.app",
@@ -13,6 +13,12 @@ const firebaseConfig = {
   appId: "1:194509041146:web:aa943d555dc067f7110843"
 };
 
+// inizializza app
 const app = initializeApp(firebaseConfig);
 
+// inizializza Firestore
+const db = getFirestore(app);
+
+// 👉 LI ESPORTIAMO
+export { app, db };
 export default app;
